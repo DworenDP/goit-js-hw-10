87d1a5d3c7393897689be44621194a88b64875ab
DworenDP <cuda.jaro@gmail.com>
2023-01-31 20:03:15 +0200

    test build

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function onInput(event) {
   if (inputCountry) {
     return getCountries(inputCountry)
       .then((data) => {
-        choseMarkup(data);
+        choiceMarkup(data);
       })
       .catch((error) => {
         Notify.failure("Oops, there is no country with that name");
@@ -33,3 +33,54 @@ function onInput(event) {
 countryListRef.style.listStyle = "none";
 countryListRef.style.margin = "0";
 countryListRef.style.padding = "10px";
+
+function choiceMarkup(countryArray) {
+  if (countryArray.length === 1) {
+    countryListRef.innerHTML = "";
+    return markupCountryItem(countryArray);
+  }
+  return Notify.info(
+    "Too many matches found. Please enter a more specific name."
+  );
+}
+
+function markupCountryItem(data) {
+  const markup = data
+    .map((element) => {
+      return `<li class="country-item">
+        <img src="${element.flags.svg}" alt="${element.name.official}" width="50" height="25" />
+        <p>${element.name.official}</p>
+        </li>`;
+    })
+    .join("");
+
+  countryListRef.innerHTML = markup;
+}
+
+function markupCountry(data) {
+  const markup = data
+    .map((element) => {
+      return `<h1>
+        <img src="${element.flags.svg}" alt="${
+        element.name.official
+      }" width="50" height="25"/>
+        ${element.name.official}</h1>
+        <ul class="country-info_list">
+        <li class="country-info_item">
+        <h2>Capital:</h2>
+        <p>${element.capital}</p>
+        </li>
+        <li class="country-info_item">
+        <h2>Population:</h2>
+        <p>${element.population}</p>
+        </li>
+        <li class="country-info_item">
+        <h2>Languages:</h2>
+        <p>${Object.values(element.languages).join(", ")}</p>
+        </li>
+        </ul>`;
+    })
+    .join("");
+
+  countryInfoRef.innerHTML = markup;
+}

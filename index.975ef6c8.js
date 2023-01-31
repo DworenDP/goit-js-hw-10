1a3012d4ff0a38dba9cf204fb4e5bad3e9d0799b
DworenDP <cuda.jaro@gmail.com>
2023-01-31 20:59:18 +0200

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -514,21 +514,20 @@ const searchRef = document.querySelector("#search-box");
 const countryListRef = document.querySelector("country-list");
 const countryInfoRef = document.querySelector("country-info");
 searchRef.addEventListener("input", (0, _lodashDebounceDefault.default)(onInput, DEBOUNCE_DELAY));
-function onInput(event) {
-    let inputCountry = event.target.value.trim();
+function onInput(e) {
+    let inputCountry = e.target.value.trim();
     if (inputCountry) return (0, _fetchScript.getCountries)(inputCountry).then((data)=>{
-        choiceMarkup(data);
+        choseMarkup(data);
     }).catch((error)=>{
         (0, _notiflix.Notify).failure("Oops, there is no country with that name");
     });
-    console.log(inputCountry);
     countryInfoRef.innerHTML = "";
     countryListRef.innerHTML = "";
 }
 countryListRef.style.listStyle = "none";
 countryListRef.style.margin = "0";
-countryListRef.style.padding = "10px";
-function choiceMarkup(countryArray) {
+countryListRef.style.padding = "8px";
+function choseMarkup(countryArray) {
     if (countryArray.length === 1) {
         countryListRef.innerHTML = "";
         return markupCountry(countryArray);
@@ -540,33 +539,35 @@ function choiceMarkup(countryArray) {
     return (0, _notiflix.Notify).info("Too many matches found. Please enter a more specific name.");
 }
 function markupCountryItem(data) {
-    const markup = data.map((element)=>{
+    const markup = data.map((el)=>{
         return `<li class="country-item">
-        <img src="${element.flags.svg}" alt="${element.name.official}" width="50" height="25" />
-        <p>${element.name.official}</p>
-        </li>`;
+            <img src="${el.flags.svg}" alt="${el.name.official}" width="40" height="20" /> 
+            <p>${el.name.official}</p>
+            </li>`;
     }).join("");
     countryListRef.innerHTML = markup;
 }
 function markupCountry(data) {
-    const markup = data.map((element)=>{
+    const markup = data.map((el)=>{
         return `<h1>
-        <img src="${element.flags.svg}" alt="${element.name.official}" width="50" height="25"/>
-        ${element.name.official}</h1>
-        <ul class="country-info_list">
+       <img src="${el.flags.svg}" alt="${el.name.official}" width="40" height="20" /> 
+            
+        ${el.name.official}
+      </h1>
+      <ul class="country-info_list">
         <li class="country-info_item">
-        <h2>Capital:</h2>
-        <p>${element.capital}</p>
+          <h2>Capital:</h2>
+          <p>${el.capital}</p>
         </li>
         <li class="country-info_item">
-        <h2>Population:</h2>
-        <p>${element.population}</p>
+          <h2>Population:</h2>
+          <p>${el.population}</p>
         </li>
         <li class="country-info_item">
-        <h2>Languages:</h2>
-        <p>${Object.values(element.languages).join(", ")}</p>
+          <h2>Languages:</h2>
+          <p>${Object.values(el.languages).join(", ")}</p>
         </li>
-        </ul>`;
+      </ul>`;
     }).join("");
     countryInfoRef.innerHTML = markup;
 }
